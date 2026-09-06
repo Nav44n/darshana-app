41ee5c5384f1936866f495a61ff20cfe7dbf13ac
Nav44n <naveenj1104@gmail.com>
2026-09-06 16:11:18 +0530

    Updates

--- assets/chevron-left-CeVS8MgY.js
+++ assets/chevron-left-CeVS8MgY.js
@@ -1,4 +1,4 @@
-import{c as o}from"./textTerminology-Luo4-oc7.js";/**
+import{c as o}from"./textTerminology-DR2xHTGq.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
